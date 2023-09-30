d694ab55223a36d3c1d5e96b67cded4cf800ba74
bjrsinge <112808041+bjrsinge@users.noreply.github.com>
2023-09-30 19:14:28 -0230

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -1,109 +1,61 @@
-using System;
 using UnityEngine;
 using Utilla;
-using UnityEngine.XR;
-using System.ComponentModel;
 using BepInEx;
 
 
 namespace Mod1
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
     [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
-    [Description("HauntedModMenu")]
-
-
-
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
         private bool inRoom;
-        private bool isRightHoldingTrigger;
-        private bool isLeftHoldingTrigger;
-        private bool willBeOn;
-
-        private void Start()
-        {
-            /* A lot of Gorilla Tag systems will not be set up when Start is called */
-            /* Put code in OnGameInitialized to avoid null references */
-
-            Utilla.Events.GameInitialized += OnGameInitialized;
-        }
 
         private void OnEnable()
         {
-            willBeOn = true;
-            HarmonyPatches.ApplyHarmonyPatches();
+
         }
 
         private void OnDisable()
         {
             GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1f, 1f, 1f);
-
-            willBeOn = false;
-
-            HarmonyPatches.RemoveHarmonyPatches();
-        }
-
-        private void OnGameInitialized(object sender, EventArgs e)
-        {
-            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
         }
 
         private void Update()
         {
-            if (inRoom && willBeOn)
+            if (inRoom)
             {
-                    InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.triggerButton, out isRightHoldingTrigger);
-                    InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.triggerButton, out isLeftHoldingTrigger);
-                    if (isRightHoldingTrigger)
-                    {
+                if (ControllerInputPoller.instance.rightGrab)
+                {
                     GorillaLocomotion.Player.Instance.transform.localScale += new Vector3(0.02f, 0.02f, 0.02f);
                     if (GorillaLocomotion.Player.Instance.transform.localScale.x > 3f)
                     {
                         GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(3f, 3f, 3f);
                     }
-                    }
+                }
 
-                    if (isLeftHoldingTrigger)
-                    {
-                        GorillaLocomotion.Player.Instance.transform.localScale -= new Vector3(0.02f, 0.02f, 0.02f);
+                if (ControllerInputPoller.instance.leftGrab)
+                {
+                    GorillaLocomotion.Player.Instance.transform.localScale -= new Vector3(0.02f, 0.02f, 0.02f);
                     if (GorillaLocomotion.Player.Instance.transform.localScale.x < 0.2f)
                     {
                         GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-
                     }
-
-                    
-
-
-
                 }
-
-
             }
         }
 
-        /* This attribute tells Utilla to call this method when a modded room is joined */
         [ModdedGamemodeJoin]
         public void OnJoin(string gamemode)
         {
-
-
             inRoom = true;
         }
 
-        /* This attribute tells Utilla to call this method when a modded room is left */
         [ModdedGamemodeLeave]
         public void OnLeave(string gamemode)
         {
             GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1f, 1f, 1f);
-
 
             inRoom = false;
         }
